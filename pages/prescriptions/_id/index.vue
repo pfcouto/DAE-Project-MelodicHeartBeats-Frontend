c1d4884b198ca5a1b605aa1430c2b739071e6c75
Mario Cruz <2191262@my.ipleiria.pt>
2021-12-21 18:21:22 +0000

    prescriptions filename altered

---
+++ pages/prescriptions/_id/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-container>
+    <div class="middleCard">
+      <h1>Prescription #{{ prescription.id }}</h1>
+      <p>Doctor: {{ prescription.doctor }}</p>
+      <p>Patient {{ prescription.patient }}</p>
+      <p>Description: {{ prescription.description }}</p>
+      <p>Start Date: {{ prescription.startDate }}</p>
+      <p>End Date: {{ prescription.endDate }}</p>
+
+      <nuxt-link to="/prescriptions">
+        <b-button variant="info"> Return</b-button>
+      </nuxt-link>
+    </div>
+  </b-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      prescription: {},
+    }
+  },
+  async created() {
+    await this.$route
+    this.$axios.$get(`/api/prescriptions/${this.$route.params.id}`).then((response) => {
+      this.prescription = response
+    })
+  },
+}
+</script>
